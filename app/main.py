0f4705b3226e468fc266a77b5f8b19e7a937b65f
ifiventured <cjtestmember01@gmail.com>
2026-01-23 12:39:21 +0000

    test server running

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # main.py assembles the API by registering each router so its endpoints are available on the server
 # creats fastapi application object
 from fastapi import FastAPI
@@ -15,3 +16,30 @@ app.include_router(groups.router, prefix="/api")
 app.include_router(watchlist.router, prefix="/api")
 
 # for the server to know which url paths exist + which functions shoukld run when urls are hit
+=======
+#from fastapi import FastAPI
+#from app.api.routes import health, auth, groups
+#from app.api.routes import watchlist
+
+
+#app = FastAPI(title="Watch With Friends API")
+
+#app.include_router(health.router, prefix="/api")
+#app.include_router(auth.router, prefix="/api")
+#app.include_router(groups.router, prefix="/api")
+#app.include_router(watchlist.router, prefix="/api")
+
+from fastapi import FastAPI
+
+ 
+
+app = FastAPI()
+
+ 
+
+@app.get("/")
+
+async def root():
+
+    return {"message": "Hello World"}
+>>>>>>> 965e0e6 (test server running)
