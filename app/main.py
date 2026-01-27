751476d4992bb20bbf25dbcb00f01a7af01f202f
lstrong-maker <lucille.strong@gmail.com>
2026-01-27 11:56:03 +0000

    adding user endpoints

--- app/main.py
+++ app/main.py
@@ -11,18 +11,42 @@
 #app.include_router(watchlist.router, prefix="/api")
 
 from fastapi import FastAPI
+from app.db.seed.connection import get_db_connection
+from app.db.seed.pw import get_connection
 
- 
+
 
 app = FastAPI()
+con = get_connection()
 
- 
+@app.get("/")
 
-#@app.get("/")
+def root():
 
-#async def root():
+    return {"message": "Hello World"}
 
-    #return {"message": "Hello World"}
+@app.get("/user/me")
+def get_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/user/{user_id}")
+def get_user(user_id: int):
+    con = get_db_connection()
+    cursor = con.cursor()
+
+    cursor.execute("SELECT user_id, username FROM Users WHERE user_id = %s", (user_id,))
+    row = cursor.fetchone()
+
+    cursor.close()
+    con.close()
+
+    if row is None:
+        return {"error": "User not found"}
+
+    return {"user_id": row[0], "username": row[1]}
+
+    #return {"user_id": user_id, "username": username}
 
 
+#, username: str | None = None
 
